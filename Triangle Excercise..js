78674a5815e47137c9ece67f93948afd17df7b01
mara5896 <59812749+mara5896@users.noreply.github.com>
2020-09-28 17:50:14 -0400

    Add files via upload

---
+++ Triangle Excercise..js
@@ -0,0 +1,11 @@
+
+   
+let number = 0;
+let hashtag = " ";
+while (number <= 6) {
+    number = number + 1;
+    hashtag = hashtag += "#";
+number + 1 === hashtag + 7;
+    console.log(hashtag);
+} 
+
